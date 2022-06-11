f37c62dfcc8634154dce2d8021d4325d769a884b
VeronikaAdamcova <adamcovaveronika@seznam.cz>
2022-06-11 18:18:08 +0200

    Add init commit

---
+++ ContactBookTestEdition/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ContactBookTestEdition.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookTestEdition.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Contacts { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ContactBookTestEdition/Data/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using ContactBookTestEdition.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookTestEdition.Data.Repository
+{
+    public class Repository : IRepository
+    {
+        private AppDbContext _ctx;
+
+        public Repository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        public void AddPerson(Person person)
+        {
+            _ctx.Contacts.Add(person);
+        }
+
+        public Person GetPerson(int id)
+        {
+            return _ctx.Contacts.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<Person> GetAllPeople()
+        {
+            return _ctx.Contacts.ToList();
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            _ctx.Contacts.Update(person);
+        }
+
+        public void DeletePerson(int id)
+        {
+            _ctx.Contacts.Remove(GetPerson(id));
+        }
+
+        public void SavePerson()
+        {
+            _ctx.SaveChanges();
+        }
+    }
+}
